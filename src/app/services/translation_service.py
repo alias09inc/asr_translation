831acc65fcb79ebacc15de9ac6db29b8f6b23e77
Kent <73377814+alias09inc@users.noreply.github.com>
2025-12-01 22:29:00 +0900

    fix name in seervices

--- src/app/services/translation_service.py
+++ src/app/services/translation_service.py
@@ -2,7 +2,7 @@ from typing import Tuple
 
 from app.registry import asr, mt
 
-def asr_and_translate_en_to_jp(audio_path: str) -> Tuple[str, str]:
+def asr_and_translate_en_to_ja(audio_path: str) -> Tuple[str, str]:
     en_text = asr().infer(audio_path)
     ja_text = mt().infer(en_text, direction="en2ja")
     return en_text, ja_text
