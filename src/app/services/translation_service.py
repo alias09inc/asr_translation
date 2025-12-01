1084928b7c72b4eb068468413ee0be7ed5688559
Kentaro <73377814+alias09inc@users.noreply.github.com>
2025-12-01 11:52:45 +0900

    add mt codes (#2)

---
+++ src/app/services/translation_service.py
@@ -0,0 +1,8 @@
+from typing import Tuple
+
+from app.registry import asr, mt
+
+def asr_and_translate_en_to_jp(audio_path: str) -> Tuple[str, str]:
+    en_text = asr().infer(audio_path)
+    ja_text = mt().infer(en_text, direction="en2ja")
+    return en_text, ja_text
